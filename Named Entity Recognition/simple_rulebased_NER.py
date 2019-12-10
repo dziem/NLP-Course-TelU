4b18d7ded0c7e9d02f9eeabf333547af7a8a22ca
dziem <dziemboh@gmail.com>
2019-12-10 20:17:26 +0700

    The Code

--- Named Entity Recognition/simple_rulebased_NER.py
+++ Named Entity Recognition/simple_rulebased_NER.py
@@ -3,9 +3,9 @@
 
 # read the file
 lines = []
-with open('kalimat_POS_NE_train.txt', 'r') as f:
+with open('kalimat_POSTag.txt', 'r') as f:
     lines = f.readlines()
-
+file = open('rulebased_output.txt','w') 
 counter_line = 0
 tokens = []
 postags = []
@@ -16,22 +16,53 @@ for line in lines:
         tokens.append(line_part[0])
         postags.append(line_part[1])
     else:
-        print(tokens)
-        print(postags)
+        #print(tokens)
+        #print(postags)
         NE_labels = []
         counter_token = 0
         prev_NE_label = ""
+        prev_token = ""
         for token in tokens:
-            if token[0].isupper() and postags[counter_token]=='NNP':
-                if prev_NE_label=="B-PER":
-                    NE_labels.append("I-PER")
+            if postags[counter_token]=='NNP':
+                if token[0].isupper():
+                    if prev_token.lower() == 'di' or prev_token.lower() == 'ke' or prev_token.lower() == 'dari':
+                        if prev_NE_label=="B-LOC" or prev_NE_label=="I-LOC":
+                            NE_labels.append("I-LOC")
+                        else:
+                            NE_labels.append("B-LOC")
+                    elif prev_NE_label=="B-PER" or prev_NE_label=="I-PER":
+                        NE_labels.append("I-PER")
+                    else:
+                        NE_labels.append("B-PER")
+                elif token.isupper():
+                    if prev_NE_label=="B-ORG" or prev_NE_label=="I-ORG":
+                        NE_labels.append("I-ORG")
+                    else:
+                        NE_labels.append("B-ORG")
                 else:
-                    NE_labels.append("B-PER")
+                    if prev_NE_label=="B-LOC" or prev_NE_label=="I-LOC":
+                        NE_labels.append("I-LOC")
+                    elif prev_NE_label=="B-PER" or prev_NE_label=="I-PER":
+                        NE_labels.append("I-PER")
+                    elif prev_NE_label=="B-ORG" or prev_NE_label=="I-ORG":
+                        NE_labels.append("I-ORG")
+                    else:
+                        NE_labels.append("O")
             else:
                 NE_labels.append("O")
             prev_NE_label = NE_labels[counter_token]
+            prev_token = token
             counter_token += 1
-        print(tokens)
-        print(NE_labels)
+        for z in range(len(tokens)):
+            file.write(tokens[z])
+            file.write('\t')
+            file.write(NE_labels[z])
+            file.write('\n')
+            if tokens[z] == '.':
+                file.write('\n')
+        #print(tokens)
+        #print(NE_labels)
         tokens = []
         postags = []
+print('finished')
+file.close()
